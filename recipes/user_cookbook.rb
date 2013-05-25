40124b1bc5d1ec94996204419d7075f4569af228
Steve Pulec <spulec@gmail.com>
2013-05-25 19:28:39 -0400

    post results back to server

--- recipes/user_cookbook.rb
+++ recipes/user_cookbook.rb
@@ -23,8 +23,17 @@ template "/var/chef/run_list.json" do
 end
 
 execute "Run chef-solo" do
-  command "chef-solo -j /var/chef/run_list.json > /var/chef/user_cookbook.log"
+  command "chef-solo -j /var/chef/run_list.json > /var/chef/user_cookbook.log; echo $? > /var/chef/user_output.txt"
   action :nothing
+  ignore_failure true
+  notifies :post, "post_results"
+end
+
+http_request "post_results" do
+  url "http://recipe-tester.com/build/status"
+  action :nothing
+  message :cookbook_name => cookbook_name, :secret_key => data_bag['s3_secret_key'], :status => File.read("/var/chef/user_output.txt")
+  headers({})
   notifies :run, "execute[shutdown]"
 end
 
