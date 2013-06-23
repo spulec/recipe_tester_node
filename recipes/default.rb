dcf4118735cead9956d8a6380143ea446c4e8ecc
Steve Pulec <spulec@gmail.com>
2013-06-23 01:20:18 -0400

    remove build-essential

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@ user = node['username']
 data_bag = data_bag_item('recipe-tester', 'config')
 
 # Install these immediately so berkshelf install will work
-['build-essential', 'libxml2-dev', 'libxslt1-dev', 'rubygems', 'git'].each do |pkg|
+['libxml2-dev', 'libxslt1-dev', 'rubygems', 'git'].each do |pkg|
   p = package pkg do
     action :nothing
     options "--fix-missing"
